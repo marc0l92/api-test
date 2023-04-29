6c7c5981e5fc53431e84daf23a0af6d931e571f3
mlucarella <Marco.LUCARELLA@amadeus.com>
2023-04-29 14:22:55 +0200

    init test

--- src/commands/init-command.test.ts
+++ src/commands/init-command.test.ts
@@ -1,12 +1,19 @@
-import { rm } from "fs-extra"
+import { rm, writeFile } from "fs-extra"
 import { initCommand } from "./init-command"
 
 const kSource = 'test/api/*.json'
 const kDestination = 'test/project'
 
 describe('init-command', () => {
-    test("initCommand", async () => {
-        await rm(kDestination, { recursive: true })
-        await initCommand(kSource, kDestination)
+    beforeEach(async () => {
+        await rm(kDestination, { recursive: true, force: true })
+    })
+    test("base", async () => {
+        // await initCommand(kSource, kDestination)
+    })
+    test("destination already exists", async () => {
+        await writeFile(kDestination, '')
+        await expect(() => initCommand(kSource, kDestination, false)).rejects
+            .toThrowError(new Error('The selected destination directory for a new project already exists'))
     })
 })
